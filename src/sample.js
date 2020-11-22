4d22b5e9ed236305aab3629c5c7f4515586dd8ef
harold Condori challco <yhon53190@gmail.com>
2020-11-22 07:02:21 -0500

    Example data JSON  from tasks

---
+++ src/sample.js
@@ -0,0 +1,21 @@
+export const tasks = [
+  {
+    _id: 0,
+    title: "Urgente",
+    description: "Hacer deploy en el servidor",
+    number: 100,
+  },
+
+  {
+    _id: 1,
+    title: "Privado",
+    description: "Hacer un backoud de la base de datos",
+    number: 90,
+  },
+  {
+    _id: 2,
+    title: "Suma urgencia",
+    description: "Hacer un prototipo de una app de viajes",
+    number: 170,
+  },
+];
